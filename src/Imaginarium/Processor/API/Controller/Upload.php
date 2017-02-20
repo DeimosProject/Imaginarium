abc9df9aa0b1b68cc32682a1a002c080c6a86a57
sttv <sttv-pc@mail.ru>
2017-02-20 09:01:03 +0300

    cross-domain try

--- src/Imaginarium/Processor/API/Controller/Upload.php
+++ src/Imaginarium/Processor/API/Controller/Upload.php
@@ -27,6 +27,19 @@ class Upload extends Controller
      */
     protected function actionDefault()
     {
+        if( !empty($_SERVER['HTTP_ORIGIN']) )
+        {
+            // Enable CORS
+            header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+            header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+            header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Range, Content-Disposition, Content-Type');
+            header('Access-Control-Allow-Credentials: true');
+        }
+        if( $_SERVER['REQUEST_METHOD'] == 'OPTIONS' )
+        {
+            exit();
+        }
+
         $user = $this->request()->attributeRequired('user');
 
         $db = new Database($this->builder);
