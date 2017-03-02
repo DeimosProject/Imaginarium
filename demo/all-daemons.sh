2515a7dcb9add7c5108603accd6ecf164c3fa499
sttv <sttv-pc@mail.ru>
2017-03-02 15:00:23 +0300

    runner fix

--- demo/all-daemons.sh
+++ demo/all-daemons.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SCREENRC='/tmp/screenrc/daemon.rc'
+TMPDIR='/tmp/imaginarium'
+SCREENRC="$TMPDIR/daemon.rc"
 DIR='/PATH/TO/PROJECT/ROOT/DIR'
 PHP='/usr/bin/php'
 LOGDIR='/var/log/imaginarium-daemon'
@@ -15,22 +16,34 @@ LOGDIR='/tmp'
 
 STORAGE="$DIR/storage"
 
-mkdir `dirname $SCREENRC -p`
+mkdir `dirname $SCREENRC` -p
 
 echo -n '' > "$SCREENRC"
 echo 'caption always "%{= 45}%{+b w}Screen: %n | %h %=%t %c"' >> "$SCREENRC"
 echo 'hardstatus alwayslastline "%-Lw%{= BW}%50>%n%f* %t%{-}%+Lw%<"' >> "$SCREENRC"
 echo 'startup_message off' >> "$SCREENRC"
 
+test -d "$TMPDIR" || mkdir "$TMPDIR" -p
+if [ ! -d "$TMPDIR" ]
+ then
+  echo "NOT CREATED!!!!!!!"
+  exit 0
+fi
 # RUN DAEMONS
 find "$STORAGE"/* -maxdepth 0 -type d | while read i
 do
     echo '' >> "$SCREENRC"
-    #echo "chdir $STORAGE/$i" >> "$SCREENRC"
     NAME=`basename $i`
-    DO="LOGDIR_=\"$LOGDIR/\`date '+%Y/%m/%d/%H-%M'\`\"; mkdir -p \"\$LOGDIR_\"; su "$PHPUSER" -s /bin/bash \"$PHP\" $DIR/Gearman.php \"$NAME\" >> \"\$LOGDIR_/$NAME.log\"; "
-    echo "screen -t \"$NAME\" while true; do $DO; done" >> "$SCREENRC"
+    COMMAND="$TMPDIR/$NAME.sh"
+    echo '#!/bin/bash' > "$COMMAND"
+    chmod 0755 "$COMMAND"
+    echo 'while true; do' >> "$COMMAND"
+    echo "LOGDIR_=\"$LOGDIR/\`date '+%Y/%m/%d/%H-%M'\`\"" >> "$COMMAND";
+    echo "mkdir -p \"\$LOGDIR_\"" >> "$COMMAND"
+    echo "su "$PHPUSER" -s /bin/bash \"$PHP\" $DIR/Gearman.php \"$NAME\" >> \"\$LOGDIR_/$NAME.log\"" >> "$COMMAND"
+    echo 'done' >> "$COMMAND"
+    echo "screen -t '$NAME' $COMMAND" >> "$SCREENRC"
 done
 
-`screen -c $SCREENRC -S imaginarium -d -m`
-rm $SCREENRC
+screen -c $SCREENRC -S imaginarium -d -m
+rm -r "$TMPDIR"
