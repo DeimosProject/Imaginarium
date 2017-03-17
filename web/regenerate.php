07b9bf8504da5d13f57c3d37f64493f1cca2abdd
Бабичев Максим <maksim.babichev95@gmail.com>
2017-03-17 12:59:48 +0300

    Update regenerate.php

--- web/regenerate.php
+++ web/regenerate.php
@@ -7,6 +7,8 @@ use Deimos\Imaginarium\Builder;
 $builder = new Builder(dirname(__DIR__));
 $request = $builder->request();
 
+header("HTTP/1.0 404 Not Found");
+
 if ($_SERVER['USER'] === 'www-data')
 {
 
